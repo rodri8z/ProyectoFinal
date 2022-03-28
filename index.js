b7da4e53dbd6c17488ae184151c462865c637dab
Rodrigo Mendoza <mendozarodrigo2002@gmail.com>
2022-03-28 20:56:41 -0300

    arreglos visuales

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 console.log("INICIO")
 
+
 //if (!sessionStorage.getItem('nombre')) {
   //let nombre = prompt("Ingrese su nombre: ")
   //sessionStorage.setItem('nombre', nombre)
 //}
 !sessionStorage.getItem('nombre') && sessionStorage.setItem('nombre', prompt("Ingrese su nombre: "))
-
+Swal.fire(
+  `bienvenido ${sessionStorage.getItem('nombre')}!`,
+  'Presiona OK para cerrar!',
+  'info'
+)
 
 let header = document.getElementsByTagName("header")
 header[0].innerHTML = `<h1>Sport Life</h1>
@@ -33,15 +38,19 @@ fetch('data.json')
       <div class='imagenProducto' style="background-image: url(${imagen})"></div>
       <div class="contenedorPrecioYCarrito">
         <h4>$${precio}</h4>
-        <div class="botonCarrito"><i class="fa fa-cart-plus"></i></i></div>
+        <div class="botonCarrito" onClick="mostrarAlerta()"><i class="fa fa-cart-plus"></i></i></div>
       </div>
     `
       contenedorTarjetas.appendChild(tarjeta)
     }
   })
 
-function agregarProductoAlCarrito() {
-  console.log("HOLA")
+function mostrarAlerta() {
+  Swal.fire(
+    'Producto agregado al carrito!',
+    'Presiona OK para cerrar!',
+    'success'
+  )
 }
 
 const botonPrueba = document.getElementById('prueba')
